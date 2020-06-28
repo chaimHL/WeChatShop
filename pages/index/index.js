4418ba5e657a1d959e6a09d9657f210fab6b6630
Chaim <1302906470@qq.com>
2020-06-28 11:57:59 +0800

    完成首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,47 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+import { request } from "../../request/index.js"
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+	data: {
+		// 轮播图数组
+		swiperList: [],
+		// 导航数组
+		catesList: [],
+		// 楼层数组
+		floorList: []
+	},
+	//事件处理函数
+	onLoad: function() {
+		this.getSwiperList()
+		this.getCatesList()
+		this.getFloorList()
+	},
+	// 获取轮播图数据
+	getSwiperList() {
+		request({
+			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+		}).then(result => {
+			this.setData({
+				swiperList: result.data.message
+			})
+		})
+	},
+	// 获取导航数据
+	getCatesList() {
+		request({
+			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+		}).then(result => {
+			this.setData({
+				catesList: result.data.message
+			})
+		})
+	},
+	// 获取楼层数据
+	getFloorList() {
+		request({
+			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
+		}).then(result => {
+			this.setData({
+				floorList: result.data.message
+			})
+		})
+	}
 })
